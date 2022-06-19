f5dcff523c519ffdc31694d25406f9c5cf1d6b48
Julia <juliasuchomlyn@gmail.com>
2022-06-19 16:59:54 +0300

    task-08

--- js/task-06.js
+++ js/task-06.js
@@ -11,28 +11,3 @@ textInput.addEventListener('blur', () => {
         textInput.classList.add('valid');
     };
 });
-    
-// function validationInput () {
-//     if (textInput.value.length !== Number(textInput.dataset.length)) {
-//         textInput.classList.remove('valid');
-//         textInput.classList.add('invalid');
-
-//     } else {
-//         textInput.classList.remove('invalid');
-//         textInput.classList.add('valid');
-//     };
-
-// };
-
-    
-//     const validation = validationInput.currentTarget;
-//     const validationLength = Number(input.dataset.length);
-//     console.log(validationLength);
-//     if (validation.value.length === validationLength) {
-//         validation.classList.remove('invalid');
-//         validation.classList.add('valid');
-//     }
-//     validation.classList.remove('valid');
-//     validation.classList.add('invalid');
-
-// });
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', hundleSubmit);
+
+function hundleSubmit(event) {
+    event.preventDefault();
+    const {
+        elements: { email, password }
+    } = event.currentTarget;
+
+    if (email.value === '' || password.value === '') {
+        return alert('Все поля должны быть заполнены')
+    }
+    const formData = {
+        email: email.value,
+        password: password.value,
+    }
+    console.log(formData)
+
+    event.currentTarget.reset();
+};
